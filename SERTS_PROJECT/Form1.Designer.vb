8a80a71f7e722edb720eb303ffb9e4b2221a1d63
davidlogan163 <david.logan@eagles.oc.edu>
2021-10-20 13:44:07 -0500

    Add project files.

---
+++ SERTS_PROJECT/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Show_Files = New System.Windows.Forms.Button()
+        Me.playButton = New System.Windows.Forms.Button()
+        Me.stopButton = New System.Windows.Forms.Button()
+        Me.resumeButton = New System.Windows.Forms.Button()
+        Me.pauseButton = New System.Windows.Forms.Button()
+        Me.selectFiles = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.File_List = New System.Windows.Forms.ListBox()
+        Me.loadFiles = New System.Windows.Forms.Button()
+        Me.ComResultTextBox = New System.Windows.Forms.TextBox()
+        Me.Disconnect = New System.Windows.Forms.Button()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.ComListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Show_Files
+        '
+        Me.Show_Files.Location = New System.Drawing.Point(341, 63)
+        Me.Show_Files.Name = "Show_Files"
+        Me.Show_Files.Size = New System.Drawing.Size(75, 23)
+        Me.Show_Files.TabIndex = 0
+        Me.Show_Files.Text = "Load Files"
+        Me.Show_Files.UseVisualStyleBackColor = True
+        '
+        'playButton
+        '
+        Me.playButton.Location = New System.Drawing.Point(164, 157)
+        Me.playButton.Name = "playButton"
+        Me.playButton.Size = New System.Drawing.Size(75, 23)
+        Me.playButton.TabIndex = 1
+        Me.playButton.Text = "Play"
+        Me.playButton.UseVisualStyleBackColor = True
+        '
+        'stopButton
+        '
+        Me.stopButton.Location = New System.Drawing.Point(282, 157)
+        Me.stopButton.Name = "stopButton"
+        Me.stopButton.Size = New System.Drawing.Size(75, 23)
+        Me.stopButton.TabIndex = 2
+        Me.stopButton.Text = "Stop"
+        Me.stopButton.UseVisualStyleBackColor = True
+        '
+        'resumeButton
+        '
+        Me.resumeButton.Location = New System.Drawing.Point(393, 156)
+        Me.resumeButton.Name = "resumeButton"
+        Me.resumeButton.Size = New System.Drawing.Size(75, 23)
+        Me.resumeButton.TabIndex = 3
+        Me.resumeButton.Text = "Resume"
+        Me.resumeButton.UseVisualStyleBackColor = True
+        '
+        'pauseButton
+        '
+        Me.pauseButton.Location = New System.Drawing.Point(508, 156)
+        Me.pauseButton.Name = "pauseButton"
+        Me.pauseButton.Size = New System.Drawing.Size(75, 23)
+        Me.pauseButton.TabIndex = 4
+        Me.pauseButton.Text = "Pause"
+        Me.pauseButton.UseVisualStyleBackColor = True
+        '
+        'selectFiles
+        '
+        Me.selectFiles.Location = New System.Drawing.Point(74, 241)
+        Me.selectFiles.Name = "selectFiles"
+        Me.selectFiles.Size = New System.Drawing.Size(75, 23)
+        Me.selectFiles.TabIndex = 6
+        Me.selectFiles.Text = "Select"
+        Me.selectFiles.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(172, 337)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Currently Playing"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(480, 337)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Time Remaining"
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM4"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(164, 353)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 17)
+        Me.ListBox1.TabIndex = 10
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(463, 353)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 17)
+        Me.ListBox2.TabIndex = 11
+        '
+        'ListView1
+        '
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(164, 186)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(419, 136)
+        Me.ListView1.TabIndex = 5
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'File_List
+        '
+        Me.File_List.FormattingEnabled = True
+        Me.File_List.Location = New System.Drawing.Point(164, 185)
+        Me.File_List.Name = "File_List"
+        Me.File_List.Size = New System.Drawing.Size(433, 134)
+        Me.File_List.TabIndex = 12
+        '
+        'loadFiles
+        '
+        Me.loadFiles.Location = New System.Drawing.Point(341, 63)
+        Me.loadFiles.Name = "loadFiles"
+        Me.loadFiles.Size = New System.Drawing.Size(75, 23)
+        Me.loadFiles.TabIndex = 0
+        Me.loadFiles.Text = "Load Files"
+        Me.loadFiles.UseVisualStyleBackColor = True
+        '
+        'ComResultTextBox
+        '
+        Me.ComResultTextBox.Location = New System.Drawing.Point(10, 107)
+        Me.ComResultTextBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComResultTextBox.Name = "ComResultTextBox"
+        Me.ComResultTextBox.Size = New System.Drawing.Size(167, 20)
+        Me.ComResultTextBox.TabIndex = 16
+        Me.ComResultTextBox.Text = "ComResultTextBox"
+        '
+        'Disconnect
+        '
+        Me.Disconnect.Location = New System.Drawing.Point(72, 84)
+        Me.Disconnect.Margin = New System.Windows.Forms.Padding(2)
+        Me.Disconnect.Name = "Disconnect"
+        Me.Disconnect.Size = New System.Drawing.Size(70, 19)
+        Me.Disconnect.TabIndex = 15
+        Me.Disconnect.Text = "Disconnect"
+        Me.Disconnect.UseVisualStyleBackColor = True
+        '
+        'Connect
+        '
+        Me.Connect.Location = New System.Drawing.Point(11, 84)
+        Me.Connect.Margin = New System.Windows.Forms.Padding(2)
+        Me.Connect.Name = "Connect"
+        Me.Connect.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Connect.Size = New System.Drawing.Size(56, 19)
+        Me.Connect.TabIndex = 14
+        Me.Connect.Text = "Connect"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'ComListBox
+        '
+        Me.ComListBox.FormattingEnabled = True
+        Me.ComListBox.Location = New System.Drawing.Point(11, 11)
+        Me.ComListBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComListBox.Name = "ComListBox"
+        Me.ComListBox.Size = New System.Drawing.Size(91, 69)
+        Me.ComListBox.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComResultTextBox)
+        Me.Controls.Add(Me.Disconnect)
+        Me.Controls.Add(Me.Connect)
+        Me.Controls.Add(Me.ComListBox)
+        Me.Controls.Add(Me.File_List)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.selectFiles)
+        Me.Controls.Add(Me.pauseButton)
+        Me.Controls.Add(Me.resumeButton)
+        Me.Controls.Add(Me.stopButton)
+        Me.Controls.Add(Me.playButton)
+        Me.Controls.Add(Me.Show_Files)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Show_Files As Button
+    Friend WithEvents playButton As Button
+    Friend WithEvents stopButton As Button
+    Friend WithEvents resumeButton As Button
+    Friend WithEvents pauseButton As Button
+    Friend WithEvents selectFiles As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents File_List As ListBox
+    Friend WithEvents loadFiles As Button
+    Friend WithEvents ComResultTextBox As TextBox
+    Friend WithEvents Disconnect As Button
+    Friend WithEvents Connect As Button
+    Friend WithEvents ComListBox As ListBox
+End Class
---
+++ SERTS_PROJECT/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.Threading
+Public Class Form1
+    Delegate Sub ListBoxDelegate(ByVal command As Integer, ByVal myStr As String)
+
+
+    Dim ListBoxDel As New ListBoxDelegate(AddressOf ListBoxDelMethod)
+
+    Dim PC_Comm As New Thread(AddressOf PC_Comm_method)
+    Dim ShowFiles As Integer = 1
+    Dim StartFileList As Integer = 2
+    Dim EndFileList As Integer = 3
+
+    Dim ShowFilesStr As String = "1"
+    Dim StartFileListStr As String = "2"
+    Dim EndFileListStr As String = "3"
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles Connect.Click
+
+        ' Connect to a selected serial port
+        If SerialPort1.IsOpen = False Then
+            ' a serial port in not already open
+            If ComListBox.SelectedItem <> Nothing Then
+                ' an item in the listbox has been selected
+                SerialPort1.PortName = ComListBox.SelectedItem
+
+                Try
+                    'try to open the selected serial port
+                    SerialPort1.Open()
+                Catch
+                    Debug.WriteLine("Failed to open serial port")
+                End Try
+                If SerialPort1.IsOpen Then
+                    ' The serial port was opened
+                    ComResultTextBox.Text = "Com port " + SerialPort1.PortName + " connected"
+                Else
+                    ' The serial port open failed
+                    ComResultTextBox.Text = "Com port " + SerialPort1.PortName + " not connected"
+                End If
+            Else
+                ComResultTextBox.Text = "Nothing selected"
+            End If
+        Else
+            ' A com port is already open
+            ComResultTextBox.Text = "Com port " + SerialPort1.PortName + " already connected"
+        End If
+
+    End Sub
+    Private Sub Disconnect_Click(sender As Object, e As EventArgs) Handles Disconnect.Click
+        ' Close the serial port if it is open
+        If SerialPort1.IsOpen Then
+            SerialPort1.Close()
+            ComResultTextBox.Text = "Com port closed"
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Try to open the seial port 
+        ComResultTextBox.Text = "Com port closed"
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComListBox.Items.Add(sp)
+        Next
+
+        ' Make this a background thread so it automatically
+        ' aborts when the main program stops.
+        PC_Comm.IsBackground = True
+        ' Set the thread priority to lowest
+        PC_Comm.Priority = ThreadPriority.Lowest
+        ' Start the thread
+        PC_Comm.Start()
+
+    End Sub
+    ' Delegate function that accesses the ListBox object
+    ' command = 2 - clear the contents of the ListBox
+    ' command = 3 - add the string to the ListBox
+    Public Sub ListBoxDelMethod(ByVal command As Integer, ByVal myStr As String)
+
+        If command = StartFileList Then
+            File_List.Items.Clear()
+        ElseIf command = EndFileList Then
+            File_List.Items.Add(myStr)
+        End If
+    End Sub
+    ' Thread that monitors the receive items on the serial port
+    Private Sub PC_Comm_method()
+        Dim str As String
+        While 1
+            If SerialPort1.IsOpen Then
+                Try
+                    str = SerialPort1.ReadLine() ' Wait for start string
+                Catch ex As Exception
+                End Try
+
+                'String.Compare return values:
+                ' Less than zero: strA precedes strB in the sort order.
+                'Zero" strA occurs in the same position as strB in the sort order.
+                'Greater than zero: strA follows strB in the sort order.
+                If Not String.Compare(str, StartFileListStr) Then
+                    ' Received a StartFileList response
+                    ' clear the list
+                    ' Use the delegate to access the ListBox
+                    File_List.Invoke(ListBoxDel, StartFileList, "")
+                    ' get next string
+                    Try
+                        str = SerialPort1.ReadLine() ' read file name
+                    Catch ex As Exception
+                    End Try
+                    While String.Compare(str, EndFileListStr)
+                        ' The read string is a file name and not the EndFileList
+                        File_List.Invoke(ListBoxDel, EndFileList, str)
+                        Try
+                            str = SerialPort1.ReadLine() ' read file name
+                        Catch ex As Exception
+                        End Try
+                    End While
+                    ' While loop ends when a 3 is received
+                End If
+            Else
+                Threading.Thread.Sleep(500)
+            End If
+        End While
+    End Sub
+
+    Private Sub Show_Files_Click(sender As Object, e As EventArgs) Handles Show_Files.Click
+        If SerialPort1.IsOpen Then
+            ' Send Show_Files command
+            SerialPort1.Write(ShowFilesStr, 0, 1)
+        End If
+    End Sub
+    Private Sub playButton_Click(sender As Object, e As EventArgs) Handles playButton.Click
+
+    End Sub
+    Private Sub pauseButton_Click(sender As Object, e As EventArgs) Handles pauseButton.Click
+
+    End Sub
+    Private Sub stopButton_Click(sender As Object, e As EventArgs) Handles stopButton.Click
+
+    End Sub
+    Private Sub resumeButton_Click(sender As Object, e As EventArgs) Handles resumeButton.Click
+
+    End Sub
+End Class
+
+
+
+
